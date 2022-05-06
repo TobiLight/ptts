f4a6299c3fa6b08f741f2661de07a178fd43fbc7
Tobilight <jgetitdonefast@gmail.com>
2022-05-06 18:45:42 +0800

    update

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -97,63 +97,7 @@ export default function Index() {
               </div>
             </div>
 
-            <div className='relative bg-gray-100 w-[100%] sm:w-[95%] md:w-[90%] lg:w-[20%] h-[350px] md:h-[100%] grid rounded drop-shadow-md'>
-              <div className='absolute right-0 top-0 bg-[#ffd800] p-[5px] max-w-[130px] rounded-bl'>
-                <p className='text-gray-600 font-bold text-xs'>Upcoming events</p>
-              </div>
-              <div className='w-full h-[200px]'>
-                <img src={summercode} alt="Symptoms and Stigma" className='object-cover w-full h-full' />
-              </div>
-              <div className='p-[10px] grid items-start justify-between ]'>
-                <p className='text-[13px] w-[100%] md:text-[13px] mb-3'>PTTS is giving kids between ages of 9 -13 an opportunity to learn Web Development this summer.</p>
-                <Form method='get' className='flex items-end'>
-                  <button className='font-bold text-[14px] lg:text-[14px] border-2 hover:p-[7px] rounded border-[#ffd800] p-[7px] transition-all ease-in hover:bg-[#ffd800] hover:text-gray-600'>Read more</button>
-                </Form>
-              </div>
-            </div>
 
-            <div className='relative bg-gray-100 w-[100%] sm:w-[95%] md:w-[90%] lg:w-[20%] h-[350px] md:h-[100%] grid rounded drop-shadow-md'>
-              <div className='absolute right-0 top-0 bg-[#ffd800] p-[5px] max-w-[130px] rounded-bl'>
-                <p className='text-gray-600 font-bold text-xs'>Upcoming events</p>
-              </div>
-              <div className='w-full h-[200px]'>
-                <img src={summercode} alt="Symptoms and Stigma" className='object-cover w-full h-full' />
-              </div>
-              <div className='p-[10px] grid items-start justify-between ]'>
-                <p className='text-[13px] w-[100%] md:text-[13px] mb-3'>PTTS is giving kids between ages of 9 -13 an opportunity to learn Web Development this summer.</p>
-                <Form method='get' className='flex items-end'>
-                  <button className='font-bold text-[14px] lg:text-[14px] border-2 hover:p-[7px] rounded border-[#ffd800] p-[7px] transition-all ease-in hover:bg-[#ffd800] hover:text-gray-600'>Read more</button>
-                </Form>
-              </div>
-            </div>
-            <div className='relative bg-gray-100 w-[100%] sm:w-[95%] md:w-[90%] lg:w-[20%] h-[350px] md:h-[100%] grid rounded drop-shadow-md'>
-              <div className='absolute right-0 top-0 bg-[#ffd800] p-[5px] max-w-[130px] rounded-bl'>
-                <p className='text-gray-600 font-bold text-xs'>Upcoming events</p>
-              </div>
-              <div className='w-full h-[200px]'>
-                <img src={summercode} alt="Symptoms and Stigma" className='object-cover w-full h-full' />
-              </div>
-              <div className='p-[10px] grid items-start justify-between ]'>
-                <p className='text-[13px] w-[100%] md:text-[13px] mb-3'>PTTS is giving kids between ages of 9 -13 an opportunity to learn Web Development this summer.</p>
-                <Form method='get' className='flex items-end'>
-                  <button className='font-bold text-[14px] lg:text-[14px] border-2 hover:p-[7px] rounded border-[#ffd800] p-[7px] transition-all ease-in hover:bg-[#ffd800] hover:text-gray-600'>Read more</button>
-                </Form>
-              </div>
-            </div>
-            <div className='relative bg-gray-100 w-[100%] sm:w-[95%] md:w-[90%] lg:w-[20%] h-[350px] md:h-[100%] grid rounded drop-shadow-md'>
-              <div className='absolute right-0 top-0 bg-[#ffd800] p-[5px] max-w-[130px] rounded-bl'>
-                <p className='text-gray-600 font-bold text-xs'>Upcoming events</p>
-              </div>
-              <div className='w-full h-[200px]'>
-                <img src={summercode} alt="Symptoms and Stigma" className='object-cover w-full h-full' />
-              </div>
-              <div className='p-[10px] grid items-start justify-between ]'>
-                <p className='text-[13px] w-[100%] md:text-[13px] mb-3'>PTTS is giving kids between ages of 9 -13 an opportunity to learn Web Development this summer.</p>
-                <Form method='get' className='flex items-end'>
-                  <button className='font-bold text-[14px] lg:text-[14px] border-2 hover:p-[7px] rounded border-[#ffd800] p-[7px] transition-all ease-in hover:bg-[#ffd800] hover:text-gray-600'>Read more</button>
-                </Form>
-              </div>
-            </div>
           </div>
         </section>
 
